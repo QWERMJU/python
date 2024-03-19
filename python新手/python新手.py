ac20740282ac5fc521f814c970f7cfa4faf90158
95062 <95062@JASON>
2024-03-19 08:28:33 +0800

    加入專案檔案。

---
+++ python新手/python新手.py
@@ -0,0 +1,15 @@
+﻿while True:
+    m = input('select123:')
+    if m=='1':
+        a, b, c, d = input('請輸入四個數字:').split()
+        print(f'result = {int(a)*int(c)-int(b)*int(d)}\n')
+    elif m=='2':
+        print('程式結束')
+        break
+    elif m=='3':
+        num = input('找因數 請輸入一個數字:')
+        print(f"{num}的因數有:")
+        for i in range(1,int(num)+1):
+            if int(num)%i == 0:
+                print(i)
+        print('\n')
